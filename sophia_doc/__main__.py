af7f3cdcfde2bfcfe8b702fd32178fc573ea2eba
st1020 <stone_1020@qq.com>
2022-04-17 17:15:45 +0800

    feat(__main__): support python 3.8+

--- sophia_doc/__main__.py
+++ sophia_doc/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 
 from docstring_parser import DocstringStyle
@@ -5,6 +6,52 @@ from docstring_parser import DocstringStyle
 from sophia_doc import ModuleNode
 from sophia_doc.utils import import_module
 from sophia_doc.builders.markdown import MarkdownBuilder
+
+if sys.version_info >= (3, 9):
+    from argparse import BooleanOptionalAction
+else:
+    from argparse import Action
+
+
+    class BooleanOptionalAction(Action):
+        def __init__(self,
+                     option_strings,
+                     dest,
+                     default=None,
+                     type=None,
+                     choices=None,
+                     required=False,
+                     help=None,
+                     metavar=None):
+
+            _option_strings = []
+            for option_string in option_strings:
+                _option_strings.append(option_string)
+
+                if option_string.startswith('--'):
+                    option_string = '--no-' + option_string[2:]
+                    _option_strings.append(option_string)
+
+            if help is not None and default is not None:
+                help += " (default: %(default)s)"
+
+            super().__init__(
+                option_strings=_option_strings,
+                dest=dest,
+                nargs=0,
+                default=default,
+                type=type,
+                choices=choices,
+                required=required,
+                help=help,
+                metavar=metavar)
+
+        def __call__(self, parser, namespace, values, option_string=None):
+            if option_string in self.option_strings:
+                setattr(namespace, self.dest, not option_string.startswith('--no-'))
+
+        def format_usage(self):
+            return ' | '.join(self.option_strings)
 
 parser = argparse.ArgumentParser(
     description='Sophia_doc is a python package to automatically generate API documents for Python modules',
@@ -24,17 +71,17 @@ parser.add_argument('--docstring-style',
                     help='Docstring style the python module used.')
 parser.add_argument('--anchor-extend',
                     type=bool,
-                    action=argparse.BooleanOptionalAction,
+                    action=BooleanOptionalAction,
                     default=False,
                     help='Add anchor to markdown title.')
 parser.add_argument('--overwrite',
                     type=bool,
-                    action=argparse.BooleanOptionalAction,
+                    action=BooleanOptionalAction,
                     default=False,
                     help='Overwrite any file in output directory.')
 parser.add_argument('--exclude-module-name',
                     type=bool,
-                    action=argparse.BooleanOptionalAction,
+                    action=BooleanOptionalAction,
                     default=False,
                     help='Write file to path which exclude module name.')
 
